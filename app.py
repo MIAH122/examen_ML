b3ca5eea5dbefbf920abd2d72b88b0494e6e48d1
MIAH122 <miahlyh1@gmail.com>
2026-05-21 12:07:52 +0300

    Prod V1

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import numpy as np
-import pickle
-import time
-
 # ============================================================
 # CONFIG
 # ============================================================
